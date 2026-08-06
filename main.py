5d0382fff7038321548c41b47fdaf1d359489cb2
shwetachoudhary11 <shwetajkb11@gmail.com>
2026-08-06 15:48:43 +0000

    added menue driven

---
+++ main.py
@@ -0,0 +1,30 @@
+
+while True:
+    print("\n========== CryptoLabX ==========")
+    print("1. Encrypt")
+    print("2. Decrypt")
+    print("3. Attack")
+    print("4. Analyze")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == "1":
+        print("\nEncrypt - Coming Soon!")
+
+    elif choice == "2":
+        print("\nDecrypt - Coming Soon!")
+
+    elif choice == "3":
+        print("\nAttack - Coming Soon!")
+
+    elif choice == "4":
+        print("\nAnalyze - Coming Soon!")
+
+    elif choice == "5":
+        print("\nThank you for using CryptoLabX.")
+        print("Exiting program...")
+        break
+
+    else:
+        print("\nInvalid choice! Please enter a number between 1 and 5.")
