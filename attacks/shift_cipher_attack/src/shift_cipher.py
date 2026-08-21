ac66d36bfa3018cf666f33345aba80de9de9b1a1
shwetachoudhary11 <shwetajkb11@gmail.com>
2026-08-21 03:31:37 +0000

    implemented shift cipher

---
+++ attacks/shift_cipher_attack/src/shift_cipher.py
@@ -0,0 +1,25 @@
+def encrypt(text, key):
+    result = ""
+
+    for ch in text:
+        if ch.isalpha():
+            start = ord('A') if ch.isupper() else ord('a')
+            result += chr((ord(ch) - start + key) % 26 + start)
+        else:
+            result += ch
+
+    return result
+
+
+def decrypt(text, key):
+    return encrypt(text, -key)
+
+
+if __name__ == "__main__":
+    text = input("Enter plaintext: ")
+    key = int(input("Enter key: "))
+
+    cipher = encrypt(text, key)
+
+    print("Encrypted text:", cipher)
+    print("Decrypted text:", decrypt(cipher, key))
